0d4afc8b9e07e481d5fc4e2371240c3d1b7ba339
hafizecim <hfz.snyl@gmail.com>
2025-09-18 18:29:21 +0300

    03_Cout_Cin

---
+++ 001_C++_Dersler_23.06.2025/03_Cout_Cin.cpp
@@ -0,0 +1,30 @@
+#include <iostream> // input output kütüphanesi
+using namespace std;
+int main(int argc, char** argv) 
+{
+	int sayi = 10;
+	cout<<"sayi degeri : ";
+    cout<<sayi; 
+    
+    cout<<"sayi degeri : "<<sayi; // yanyana yazdırma
+    
+    
+    int a = 10;
+    int b = 20;
+    cout<<"toplam : "<< a + b;
+    
+    
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    cout<<"toplam : "<< c; // toplam : 30
+    cout<< a << "+" << b << "=" << c; // a+b=30
+    
+    int sayi2;
+    cout << "4 basamakli bir sayi giriniz : ";
+    cin  >> sayi5; // character in
+    cout << "Girilen sayi :" << sayi5;
+
+    return 0;
+    
+}

a8aeaf8b2f953365cc165fc66b5b9bd8d1da5959
hafizecim <hfz.snyl@gmail.com>
2025-09-18 18:28:31 +0300

    02_Degisken_Tanimlama

---
+++ 001_C++_Dersler_23.06.2025/02_Degisken_Tanimlama.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	// DEÐÝÞKEN TANIMLAMA - YÖNTEM 1
+	
+	int sayi1 = 10;
+	
+	// DEÐÝÞKEN TANIMLAMA - YÖNTEM 2
+	
+	int sayi2;
+	int sayi3 = 10;
+	sayi2 = 20;
+	
+	// DEÐÝÞKEN TANIMLAMA - YÖNTEM 3
+	
+	int sayi4 = 10, sayi5 = 10;
+	float sayi6 = 1.2;
+	
+	
+	// DEÐÝÞKEN TANIMLAMA - YÖNTEM 4 
+	
+		int sayi7, sayi8;
+		sayi7 = 10;
+		sayi8 = 10;
+		
+	// DEÐÝÞKEN TANIMLAMA - YÖNTEM 5
+	
+	int sayi9 = 10, sayi10;
+	sayi10 = 10;
+	
+	return 0;
+}

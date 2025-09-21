03dd35abd6f37b750e2c9cb5a8d4ca37688cd952
hafizecim <hfz.snyl@gmail.com>
2025-09-21 18:42:47 +0300

    05_BankaMatik

---
+++ 003_C++_Dersler_20.09.2025/main1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) {
+	int sayi = 1;
+	if(sayi==1){
+		cout<<"Bir";
+	}
+	else if(sayi==2){
+		cout<<"Iki";
+	}
+	else{
+		cout<<"Bir yada iki deðidir";
+	}
+	return 0;
+}

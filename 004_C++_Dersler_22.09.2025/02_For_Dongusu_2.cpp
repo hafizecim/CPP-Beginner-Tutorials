791c42dd2da4a835dd64beed12f73d4ee9a36bc6
hafizecim <hfz.snyl@gmail.com>
2025-09-22 20:24:52 +0300

    02_For_Dongusu_2

---
+++ 004_C++_Dersler_22.09.2025/02_For_Dongusu_2.cpp
@@ -0,0 +1,16 @@
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  for(int i=0; i<10; i++){ // önce arttýr daha sonra kontrol et
+    cout << i << " ";  // 0 2 4 6 8
+    i++;
+  }
+  // çýktý : 0 2 4 6 8
+  return 0;
+}
+
+

38183cd456a91e3c16a7eafd17f907698b849829
hafizecim <hfz.snyl@gmail.com>
2025-09-24 18:36:54 +0300

    05_For_Dongusu_5

---
+++ 004_C++_Dersler_22.09.2025/05_For_Dongusu_5.cpp
@@ -0,0 +1,18 @@
+
+/* 
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) { 
+  for(int i=0; i<=100; i+=5){      // |  0  6  12  18             
+  	++i; 	                      // |   1  7  13  19              
+    cout << ++i <<" ";            // |   2  8  14  20    // ilk aþmada i++ --> önce kullan sonra arttýr -2(-1)
+    i--; 						  // |   1  7  13  19
+  }
+  // çýktý : 2  8  14  20 
+  // 
+  // i nin son deðeri 102 dir.
+  return 0;
+}
+*/

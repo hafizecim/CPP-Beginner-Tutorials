7908f28b8e3b117e08559bef5b553144ec7ebc8e
hafizecim <hfz.snyl@gmail.com>
2025-09-27 19:57:05 +0300

    10_For_Dongusu_Break

---
+++ 004_C++_Dersler_22.09.2025/10_For_Dongusu_Break.cpp
@@ -0,0 +1,17 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+  for (int i = 1; i < 8; i++) {
+    if (i == 5) {
+      break;
+    }
+    cout << i << "";
+  }
+  
+  return 0;
+}
+*/

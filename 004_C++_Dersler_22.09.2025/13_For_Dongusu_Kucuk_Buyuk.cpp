79db2009507a18b1785765f9a9b7fe4f49c2d999
hafizecim <hfz.snyl@gmail.com>
2025-09-27 20:01:52 +0300

    13_For_Dongusu_Kucuk_Buyuk

---
+++ 004_C++_Dersler_22.09.2025/13_For_Dongusu_Kucuk_Buyuk.cpp
@@ -0,0 +1,27 @@
+/*
+// klavyeden girilen 10 adet tam sayý arasýnda en büyüðnu ve en küçüðünü bula ve ekranda bastýran
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  int sayi;
+  int enBuyuk = sayi, enBucuk = sayi;
+  for (int i = 1; i <= 10; i++) {
+    cout << i << ". sayiyi giriniz:";
+    cin >> sayi;
+    if (enBuyuk >= sayi1) {
+      enBuyuk = sayi;
+    }
+    if (enKucuk <= sayi) {
+      enKucuk = sayi;
+    }
+  }
+  cout << " En büyük sayi : " << enBuyuk << endl;
+  cout << " En küçük sayi : " << enKucuk << endl;
+
+  return 0;
+}
+*/
+
+

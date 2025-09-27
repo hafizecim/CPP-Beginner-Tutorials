babd6718ae78fa27029c04ba07450f8468b01ad5
hafizecim <hfz.snyl@gmail.com>
2025-09-27 19:57:53 +0300

    11_For_Dongusu_Continue

---
+++ 004_C++_Dersler_22.09.2025/11_For_Dongusu_Continue.cpp
@@ -0,0 +1,17 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+  for (int i = 1; i < 10; i++) {
+    if (i == 5) {
+      continue; // -->  5 i atla
+    }
+    cout << i << ""; // 0 1 2 3 4 6 7 8 9 --> 5 i atlar
+  }
+  
+  return 0;
+}
+*/

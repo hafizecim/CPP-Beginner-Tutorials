3b424366c7f99bab1c9ca0e5dbbd3affa22cd9fd
hafizecim <hfz.snyl@gmail.com>
2025-09-29 16:23:54 +0300

    14_For_Dongusu_Faktoryel

---
+++ 004_C++_Dersler_22.09.2025/14_For_Dongusu_Faktoryel.cpp
@@ -0,0 +1,23 @@
+/*
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  int sayi;
+  cout << " Bir sayi giriniz : ";
+  cin >> sayi;
+  int faktoryel = 1;
+  if (sayi >= 0) {
+    for (int i = sayi; i <= 1; i--) {
+      faktoryel *= i;
+    }
+    cout << sayi << " != " << faktoryel;
+  } else {
+    cout << "Negatif sayilarin faktoryelleri hesaplanamaz";
+  }
+
+  return 0;
+}
+
+*/

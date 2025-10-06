8c7f125ce8882dc930ec56b26f543a8c975c4005
hafizecim <hfz.snyl@gmail.com>
2025-10-06 19:38:00 +0300

    04_Cok_Boyutlu_Diziler_Ornek34

---
+++ 007_C++_Dersler_04.10.2025/04_Cok_Boyutlu_Diziler_Ornek4.cpp
@@ -0,0 +1,48 @@
+/* Dizi çýktýsý aþaðýdaki matris olan C++ kodunu yazýnýz:
+
+0       0       0       0       1
+0       0       0       1       0
+0       0       1       0       0
+0       1       0       0       0
+1       0       0       0       0
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+// main.cpp tarafýndan çaðrýlacak örnek fonksiyon
+void cokBoyutluDizilerOrnek4() {
+
+  // 5x5 boyutunda bir çok boyutlu dizi (matris) tanýmlanýyor
+  // int dizi[satir][sutun];
+  int dizi[5][5]; // 5 satýr, 5 sütunluk matris
+
+  // DÝZÝYE DEÐER ATAMA
+  // Eðer satýr ve sütun indeksleri eþitse (yani köþegen), 1 atanýr
+  // Diðer hücrelere ise 0 atanýr
+  for (int i = 0; i < 5; i++) {        // Satýrlarý dolaþ
+    for (int j = 0; j < 5; j++) {      // Sütunlarý dolaþ
+      if (i + j == 4) {
+        dizi[i][j] = 1;               // Köþegen elemanlara 1 ata
+      } else {
+        dizi[i][j] = 0;               // Diðer elemanlara 0 ata
+      }
+    }
+  }
+
+  // DÝZÝYÝ EKRANA YAZDIRMA
+  // Matris biçiminde çýktý alýnýr
+  for (int i = 0; i < 5; i++) {        // Satýrlarý dolaþ
+    for (int j = 0; j < 5; j++) {      // Sütunlarý dolaþ
+      cout << dizi[i][j] << "\t";     // Elemaný yazdýr (sekme ile hizalý)
+    }
+    cout << endl;                     // Her satýrdan sonra yeni satýra geç
+  }
+}
+
+
+
+
+
